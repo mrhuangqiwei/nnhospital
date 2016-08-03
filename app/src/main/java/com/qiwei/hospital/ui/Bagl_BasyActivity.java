a424727b3c98b2002e196c67ee9800ef719e9f51
mrhuangqiwei <973414519@qq.com>
2016-08-03 17:26:37 +0800

    11

---
+++ app/src/main/java/com/qiwei/hospital/ui/Bagl_BasyActivity.java
@@ -0,0 +1,36 @@
+package com.qiwei.hospital.ui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.qiwei.hospital.ActivityHelper.BaseActivity;
+import com.qiwei.hospital.R;
+
+/**
+ * 开发时间：2016.8.2
+ * 开发人：黄启位
+ * 功能描述：病案首页
+ */
+public class Bagl_BasyActivity extends BaseActivity implements View.OnClickListener{
+
+    @Override
+    protected void initEnvironment() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return (R.layout.activity_bagl__basy);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
